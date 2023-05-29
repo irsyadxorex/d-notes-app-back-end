e323d9c50e429bd06c56a7dc5587804b25dda4d4
irsyad-kd <irsyad.alfahriza@katadata.co.id>
2023-05-29 10:57:03 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const {
+  addNoteHandler,
+  getAllNoteHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNoteHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
